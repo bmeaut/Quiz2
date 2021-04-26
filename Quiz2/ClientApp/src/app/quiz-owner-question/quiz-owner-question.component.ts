e4974d7d6586682a9ee14157a1d5c093f46a1bed
Roland Toth <troland703@gmail.com>
2021-04-26 19:03:10 +0200

    Question edit

--- Quiz2/ClientApp/src/app/quiz-owner-question/quiz-owner-question.component.ts
+++ Quiz2/ClientApp/src/app/quiz-owner-question/quiz-owner-question.component.ts
@@ -23,10 +23,10 @@ export class QuizOwnerQuestionComponent implements OnInit {
     position: 1,
     points: 5,
     answers:
-    [{id: 1, isCorrect: true, text: "Válasz1", questionId: 1},
-     {id: 2, isCorrect: false, text: "Válasz2", questionId: 1},
-     {id: 3, isCorrect: false, text: "Válasz3", questionId: 1},
-     {id: 4, isCorrect: false, text: "Válasz4", questionId: 1}]};
+    [{id: 1, correct: true, text: "Válasz1", questionId: 1},
+     {id: 2, correct: false, text: "Válasz2", questionId: 1},
+     {id: 3, correct: false, text: "Válasz3", questionId: 1},
+     {id: 4, correct: false, text: "Válasz4", questionId: 1}]};
 
   constructor(public gameService: GamesService) { }
 
