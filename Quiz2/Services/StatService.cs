f3b161beadd97de8a851d6827f9aad644172da83
pet339 <petra.pristyak@gmail.com>
2021-05-03 18:09:44 +0200

    Felhasználó által indított játékok listázása

--- Quiz2/Services/StatService.cs
+++ Quiz2/Services/StatService.cs
@@ -1,6 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Quiz2.Data;
+using Quiz2.Helper;
+using Quiz2.Models;
 
 namespace Quiz2.Services
 {
@@ -21,5 +26,25 @@ namespace Quiz2.Services
                 .Where(userAnswer => userAnswer.Game.Id == gameId)
                 .Count(userAnswer => userAnswer.Answer.Id == answerId);
         }
+
+        public List<Game> GetOwnedGameHistory(string ownerId)
+        {
+            return _context.Games
+                .Include(game => game.Owner)
+                .Include(game => game.Status)
+                .Include(game => game.Quiz)
+                .Include(game => game.JoinedUsers)
+                .Where(game => game.Owner.Id == ownerId)
+                .ToList();
+        }
+/*
+        public int GetUserPointsInGame(int gameId, string userId)
+        {
+            return _context.Games
+                .Include(game => game.JoinedUsers)
+                .Where(game => game.Id == gameId && game.JoinedUsers.FirstOrDefault(user => user.Id == userId))
+
+        }
+*/
     }
 }
